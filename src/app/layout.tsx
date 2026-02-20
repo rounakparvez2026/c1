4984b3fbf1f2bca1660e9838ce101f1b52e9dd71
xcfio <119097812+xcfio@users.noreply.github.com>
2026-02-20 17:40:05 +0600

    feat: add custom analytics script to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import { Analytics } from "@vercel/analytics/next"
 import { Metadata } from "next"
+import Script from "next/script"
 import "./globals.css"
 
 export const metadata: Metadata = {
@@ -11,6 +12,13 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
     return (
         <html lang="en">
+            <head>
+                <Script
+                    defer
+                    src="https://analytics-xcfio.netlify.app/script.js"
+                    data-website-id="cdcd5bc6-0f04-421e-8d3e-f09a2644085f"
+                />
+            </head>
             <body className={`antialiased`}>{children}</body>
             <Analytics />
         </html>
