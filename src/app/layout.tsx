8f2c730ff7a66f43fb93000222e8b2e084b6580f
xcfio <119097812+xcfio@users.noreply.github.com>
2026-04-06 06:51:50 +0600

    fix: add suppressHydrationWarning to html tag in RootLayout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
     return (
-        <html lang="en">
+        <html lang="en" suppressHydrationWarning>
             <body className={`antialiased`}>{children}</body>
             <SpeedInsights />
             <Analytics />
